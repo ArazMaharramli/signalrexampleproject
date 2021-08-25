74839e8737aa00d55701c10380736432ec4192f0
arazmaharramli <araz.maharramli@novum.az>
2021-08-25 21:20:53 +0400

    create basic chat

---
+++ SignalRExampleProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SignalRExampleProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
